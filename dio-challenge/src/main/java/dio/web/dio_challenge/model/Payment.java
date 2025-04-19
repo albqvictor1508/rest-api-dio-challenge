aff6f9dd9fb35b094dbcf371ae77dbfa9e9c4a87
albqvictor1508 <albq.victor@gmail.com>
2025-04-19 20:05:14 -0300

    chore: payment entity v1 completed

--- dio-challenge/src/main/java/dio/web/dio_challenge/model/Payment.java
+++ dio-challenge/src/main/java/dio/web/dio_challenge/model/Payment.java
@@ -6,13 +6,20 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Payment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Double price;
-    private PaymentEnum paymentType;
-    private IPayment paymentStrategy;
+    private String paymentType;
 }
