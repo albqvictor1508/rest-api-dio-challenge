128ead58d32fddd58efd1b082a76615e09588609
albqvictor1508 <albq.victor@gmail.com>
2025-04-19 19:39:28 -0300

    initial commit

---
+++ dio-challenge/src/main/java/dio/web/dio_challenge/DioChallengeApplication.java
@@ -0,0 +1,13 @@
+package dio.web.dio_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioChallengeApplication.class, args);
+	}
+
+}
---
+++ dio-challenge/src/main/java/dio/web/dio_challenge/model/Payment.java
@@ -0,0 +1,5 @@
+package dio.web.dio_challenge.model;
+
+public interface Payment {
+    boolean process(PaymentData data);
+}
