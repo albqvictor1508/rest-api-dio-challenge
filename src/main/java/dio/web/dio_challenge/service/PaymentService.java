ee482e4fe042889c43e26925e19c5518af85dc20
albqvictor1508 <albq.victor@gmail.com>
2025-04-19 22:25:59 -0300

    api finished

--- src/main/java/dio/web/dio_challenge/service/PaymentService.java
+++ src/main/java/dio/web/dio_challenge/service/PaymentService.java
@@ -3,9 +3,11 @@ package dio.web.dio_challenge.service;
 import dio.web.dio_challenge.model.Payment;
 import dio.web.dio_challenge.repository.ClientRepository;
 import dio.web.dio_challenge.repository.PaymentRepository;
+import dio.web.dio_challenge.strategy.Billet;
 import dio.web.dio_challenge.strategy.CreditCard;
 import dio.web.dio_challenge.strategy.PaymentStrategy;
 
+import dio.web.dio_challenge.strategy.Pix;
 import dio.web.dio_challenge.types.PaymentEnum;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -35,12 +37,23 @@ public class PaymentService {
     public Payment realizePayment(Long id) {
         Payment p = paymentRepo.findById(id).orElseThrow(() -> new RuntimeException("user not finded"));
         boolean result = setPaymentStrategy(p);
+        if(!result) throw new RuntimeException("Error on realize payment");
         p.setPayed(true);
         return p;
     }
 
-    private PaymentStrategy setPaymentStrategy(Payment payment) {
+    private boolean setPaymentStrategy(Payment payment) {
         String label = payment.getPaymentType();
         String paymentType = Arrays.stream(PaymentEnum.values()).filter(p -> p.getLabel().equalsIgnoreCase(label)).findFirst().get().toString();
+        PaymentStrategy ps = null;
+
+        switch (paymentType) {
+            case "credit card" -> ps = CreditCard.getInstance();
+            case "billet" -> ps = new Billet();
+            case "pix" -> ps = new Pix();
+            default -> throw new RuntimeException("only 'credit card', 'billet' and 'pix' are required");
+        }
+
+        return ps.process(payment);
     }
 }
